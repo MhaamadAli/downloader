40921876f10aa1420e2a3876733e78f41407d85c
MhaamdAli <m.alialaawar@gmail.com>
2025-07-31 02:53:17 +0300

    fix: 🐛 updated download modlel

--- src/models/download.rs
+++ src/models/download.rs
@@ -58,13 +58,54 @@ impl DownloadProgress {
     
     /// Format download speed for display
     pub fn speed_string(&self) -> String {
-        // TODO: Implement speed formatting
-        todo!("Implement speed formatting")
+        if self.download_speed < 1024.0 {
+            format!("{:.1} B/s", self.download_speed)
+        } else if self.download_speed < 1024.0 * 1024.0 {
+            format!("{:.1} KB/s", self.download_speed / 1024.0)
+        } else if self.download_speed < 1024.0 * 1024.0 * 1024.0 {
+            format!("{:.1} MB/s", self.download_speed / (1024.0 * 1024.0))
+        } else {
+            format!("{:.1} GB/s", self.download_speed / (1024.0 * 1024.0 * 1024.0))
+        }
     }
     
     /// Format ETA for display
     pub fn eta_string(&self) -> String {
-        // TODO: Implement ETA formatting
-        todo!("Implement ETA formatting")
+        if self.eta_seconds == 0 {
+            "Unknown".to_string()
+        } else if self.eta_seconds < 60 {
+            format!("{}s", self.eta_seconds)
+        } else if self.eta_seconds < 3600 {
+            let minutes = self.eta_seconds / 60;
+            let seconds = self.eta_seconds % 60;
+            format!("{}m {}s", minutes, seconds)
+        } else {
+            let hours = self.eta_seconds / 3600;
+            let minutes = (self.eta_seconds % 3600) / 60;
+            format!("{}h {}m", hours, minutes)
+        }
+    }
+    
+    /// Format total size for display
+    pub fn size_string(&self) -> String {
+        Self::format_bytes(self.total_size)
+    }
+    
+    /// Format downloaded size for display
+    pub fn downloaded_string(&self) -> String {
+        Self::format_bytes(self.downloaded_size)
+    }
+    
+    /// Format bytes into human readable string
+    pub fn format_bytes(bytes: u64) -> String {
+        if bytes < 1024 {
+            format!("{} B", bytes)
+        } else if bytes < 1024 * 1024 {
+            format!("{:.1} KB", bytes as f64 / 1024.0)
+        } else if bytes < 1024 * 1024 * 1024 {
+            format!("{:.1} MB", bytes as f64 / (1024.0 * 1024.0))
+        } else {
+            format!("{:.1} GB", bytes as f64 / (1024.0 * 1024.0 * 1024.0))
+        }
     }
 }
